f211ed3188f22e8d55bfd53fea4f2d80c15cb5c3
Diego Fabricio <difagume@gmail.com>
2018-03-03 12:05:44 -0500

    Servicio de bloqueo - CanActivate - AuthGuard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { APP_ROUTING } from './app.routes';
 
 // Servicios
 import { AuthService } from './services/auth.service';
+import { AuthGuardService } from './services/auth-guard.service';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { AuthService } from './services/auth.service';
     APP_ROUTING
   ],
   providers: [
-    AuthService
+    AuthService,
+    AuthGuardService
   ],
   bootstrap: [AppComponent]
 })
